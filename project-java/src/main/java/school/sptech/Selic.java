528c3a7d6806c89c60b9b65ff521d40009be338a
Felipe-Macedo-Sptech <felipe.correia@sptech.school>
2025-11-12 19:43:26 -0300

    ajustando a selic para o novo mer

--- project-java/src/main/java/school/sptech/Selic.java
+++ project-java/src/main/java/school/sptech/Selic.java
@@ -9,12 +9,24 @@ public class Selic {
         return idtblSelic;
     }
 
+    public void setIdtblSelic(Integer idtblSelic) {
+        this.idtblSelic = idtblSelic;
+    }
+
     public Double getValorTaxa() {
         return valorTaxa;
     }
 
+    public void setValorTaxa(Double valorTaxa) {
+        this.valorTaxa = valorTaxa;
+    }
+
     public String getDtApuracao() {
         return dtApuracao;
+    }
+
+    public void setDtApuracao(String dtApuracao) {
+        this.dtApuracao = dtApuracao;
     }
 
     @Override
