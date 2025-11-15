7f521ff7b0342457409e15e8902abb81e3b34291
Felipe-Macedo-Sptech <felipe.correia@sptech.school>
2025-11-15 04:06:01 -0300

    inserção de todos de todas bases de dados

--- project-java/src/main/java/school/sptech/Main.java
+++ project-java/src/main/java/school/sptech/Main.java
@@ -13,9 +13,11 @@ public class Main {
         }
         LerPersistirDados persistirDados = new LerPersistirDados();
 
-        //persistirDados.inserirDadosPib("2t2025_tabelas_site-pib-trimestral.xlsx");
-        //persistirDados.inserirDadosPopulacao("2t2025_tabelas_site-pib-trimestral.xlsx");
-        //persistirDados.inserirDadosPopulacao("populcao.xlsx");
+        persistirDados.inserirDadosInflacao("inflacao.xlsx");
+        persistirDados.inserirDadosSelic("selic.xlsx");
+        persistirDados.inserirDadosPopulacao("populcao.xlsx");
+        persistirDados.inserirDadosPib("2t2025_tabelas_site-pib-trimestral.xlsx");
+        persistirDados.inserirDadosPibSetor("2t2025_tabelas_site-pib-trimestral.xlsx");
         persistirDados.inserirDadosPibRegionalSP("sp_pib.xlsx");
     }
 }
