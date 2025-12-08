9ec9cc7534397eec8ad7f48e97a5ce131fff80bc
Felipe-Macedo-Sptech <felipe.correia@sptech.school>
2025-12-08 20:02:36 -0300

    ajuste conecxão do container

--- project-java/src/main/java/school/sptech/Main.java
+++ project-java/src/main/java/school/sptech/Main.java
@@ -24,6 +24,7 @@ public class Main {
             e.printStackTrace();
         }
 
+
         LerPersistirDados persistirDados = new LerPersistirDados();
 
         persistirDados.inserirDadosInflacao("inflacao.xlsx");
