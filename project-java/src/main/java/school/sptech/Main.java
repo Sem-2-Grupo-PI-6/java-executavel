d4e32d49af093ed8842618d306a4cf255ed2e909
Felipe-Macedo-Sptech <felipe.correia@sptech.school>
2025-12-11 00:36:49 -0300

    ajustando o trativa de erro de conexão

--- project-java/src/main/java/school/sptech/Main.java
+++ project-java/src/main/java/school/sptech/Main.java
@@ -6,15 +6,6 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
 
-        Conexao conexao = new Conexao();
-
-        try (Connection conn = conexao.getConexao().getConnection()) {
-            System.out.println("Conexão estabelecida com sucesso!");
-        } catch (Exception e) {
-            System.err.println("Falha na conexão: " + e.getMessage());
-            e.printStackTrace();
-        }
-
 
         LerPersistirDados persistirDados = new LerPersistirDados();
 
